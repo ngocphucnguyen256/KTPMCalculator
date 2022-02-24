126a17f7aea3a761a4a64a3b88cb254eb9e7c379
ngocphucnguyen256 <ngocphuc.nguyen256@gmail.com>
2022-02-24 10:49:32 +0700

    add button

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -83,6 +83,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "Cong";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
